a08c0e5bcecbfe114d469ba58458f3ae38418149
John <johnngo@Johns-MacBook-Pro.local>
2017-12-18 12:44:23 +0800

    commit project

---
+++ poi_id_enron.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python
+'''
+Please refer to the accompany notebook.
+
+   enronProject.ipynb
+'''
+import sys
+import pickle
+import numpy as np 
+import pandas as pd 
+
+sys.path.append("../tools/")
+
+from feature_format import featureFormat, targetFeatureSplit
+from tester import dump_classifier_and_data
+
+### Task 1: Select what features you'll use.
+### features_list is a list of strings, each of which is a feature name.
+### The first feature must be "poi".
+
+# Load the dictionary containing the dataset
+with open("final_project_dataset.pkl", "r") as data_file:
+    enronData = pickle.load(data_file)
+
+# Convert data dictionary into a pandas dataframe, for ease of manipulation
+
+df = pd.DataFrame.from_dict(enronData, orient = 'index')
+
+df.replace('NaN', 0, inplace = True)
+
+# features_list = ['poi','salary'] # You will need to use more features
+
+
+### Task 2: Remove outliers
+
+df = df.drop(['TOTAL', 'THE TRAVEL AGENCY IN THE PARK'])
+
+
+### Task 3: Create new feature(s)
+
+df.replace(0, np.nan, inplace = True)
+
+df['f_to_poi'] = df['from_this_person_to_poi']/df['from_messages']
+
+df['f_from_poi'] = df['from_poi_to_this_person']/df['to_messages']
+
+
+# Replace np.nan with NaN (for compatibility with feature_format.py)
+
+df.replace(np.nan, 'NaN', inplace = True)
+
+# create a dictionary from the dataframe
+df_dict = df.to_dict('index')
+
+### Store to my_dataset for easy export below.
+my_dataset = df_dict
+
+# features_list is a list of strings, each of which is a feature name.
+# The first feature must be "poi".
+
+features_list = ['poi','salary', 'deferral_payments', 'total_payments', 
+'loan_advances', 'bonus', 'restricted_stock_deferred', 'deferred_income', 
+'total_stock_value', 'expenses', 'exercised_stock_options', 'other',
+'long_term_incentive', 'restricted_stock', 'director_fees','to_messages',
+'from_poi_to_this_person', 'from_messages','from_this_person_to_poi', 
+'shared_receipt_with_poi','f_from_poi','f_to_poi']
+
+### Extract features and labels from dataset for local testing
+# data = featureFormat(my_dataset, features_list, remove_NaN=True, remove_all_zeroes=True, 
+#                      remove_any_zeroes=True, sort_keys = True)
+
+data = featureFormat(my_dataset, features_list, sort_keys = True)
+
+labels, features = targetFeatureSplit(data)
+
+### Task 4: Try a varity of classifiers
+### Please name your classifier clf for easy export below.
+### Note that if you want to do PCA or other multi-stage operations,
+### you'll need to use Pipelines. For more info:
+### http://scikit-learn.org/stable/modules/pipeline.html
+
+# Provided to give you a starting point. Try a variety of classifiers.
+from sklearn.tree import DecisionTreeClassifier
+
+clf = DecisionTreeClassifier(random_state=42)
+
+
+# clf = DecisionTreeClassifier(class_weight=None, criterion='gini', max_depth=4,
+#             max_features=7, max_leaf_nodes=None, min_impurity_decrease=0.0,
+#             min_impurity_split=None, min_samples_leaf=1,
+#             min_samples_split=2, min_weight_fraction_leaf=0.0,
+#             presort=False, random_state=None, splitter='best')
+
+# clf = DecisionTreeClassifier(class_weight=None, criterion='gini', max_depth=3,
+#             max_features=7, max_leaf_nodes=8, min_impurity_decrease=0.0,
+#             min_impurity_split=None, min_samples_leaf=1,
+#             min_samples_split=4, min_weight_fraction_leaf=0.0,
+#             presort=False, random_state=None, splitter='random')
+
+
+# clf = DecisionTreeClassifier(class_weight=None, criterion='entropy', max_depth=4,
+#             max_features=2, max_leaf_nodes=None, min_impurity_decrease=0.0,
+#             min_impurity_split=None, min_samples_leaf=1,
+#             min_samples_split=6, min_weight_fraction_leaf=0.0,
+#             presort=False, random_state=None, splitter='best')
+
+clf.fit(features,labels)
+
+### Task 5: Tune your classifier to achieve better than .3 precision and recall 
+### using our testing script. Check the tester.py script in the final project
+### folder for details on the evaluation method, especially the test_classifier
+### function. Because of the small size of the dataset, the script uses
+### stratified shuffle split cross validation. For more info: 
+### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
+
+# Example starting point. Try investigating other evaluation techniques!
+# from sklearn.cross_validation import train_test_split
+# features_train, features_test, labels_train, labels_test = \
+#     train_test_split(features, labels, test_size=0.3, random_state=42)
+
+### Task 6: Dump your classifier, dataset, and features_list so anyone can
+### check your results. You do not need to change anything below, but make sure
+### that the version of poi_id.py that you submit can be run on its own and
+### generates the necessary .pkl files for validating your results.
+
+dump_classifier_and_data(clf, my_dataset, features_list)
+
